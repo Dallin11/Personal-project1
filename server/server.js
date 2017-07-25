661b8af3c6b7c9eca3e066a76ae5358917302c80
Dallin Anderson <andersondallin11@gmail.com>
2017-07-25 16:30:15 -0600

    fixed user problem

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ app.use(express.static(__dirname + "./../public"))
 passport.use(new Auth0Strategy({
     domain: process.env.domain,
     clientID:process.env.clientID,
-    clientSecret: process.env.secret,
+    clientSecret: process.env.clientSecret,
     callbackURL: "/auth/callback"
 }, function (assesToken, refreshToken, extraParams, profile, done) {
 
