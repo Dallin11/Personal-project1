9b5639fdb1141ad706e3e57bb2782d286aa0928c
Dallin Anderson <andersondallin11@gmail.com>
2017-07-25 12:08:16 -0600

    FIxed add events sql

--- db/addEvent.sql
+++ db/addEvent.sql
@@ -1,2 +1,2 @@
-insert into events (userId integer references users(userId),title, color, start_time, end_time) values ($1, $2, $3, $4, $5)
+insert into events (userId integer references users(userId),title, color, start_time, end_time) values ($1, $2, $3, $4)
 returning *
