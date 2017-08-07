c9c954e1e3a780e8e2d9919c076a12794466c55f
Dallin Anderson <andersondallin11@gmail.com>
2017-08-07 14:00:06 -0600

    npm i package memory store

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     secret: process.env.secret,
-    store: new MemoryStore(options)
+    store: new MemoryStore
 }))
 app.use(passport.initialize())
 app.use(passport.session())
